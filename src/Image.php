d2ce363a83c25cab7cc405f12ae0c9331d6e7863
sholomka <sholomka@gmail.com>
2016-10-24 19:48:06 +0300

    add tests

--- src/Image.php
+++ src/Image.php
@@ -4,11 +4,63 @@ namespace Sholomka\Image;
 
 use Sholomka\Image\Exceptions\ImageException;
 
-
+/**
+ * Package for the Composer, which will deal with the fact that from
+ * the remote host to upload images and save them on the file system.
+ *
+ * Class Image
+ * @package Sholomka\Image
+ */
 class Image
 {
-    private $imageName;
+    /**
+     * Regex for parsing pictures
+     */
+    const REGEX = '/<\s*img[^>]*src=[\"|\'](.*?)[\"|\'][^>]*\/*>/i';
+
+    /**
+     * The default directory for storing pictures
+     *
+     * @var string
+     */
     private $imageDirectory;
+
+    /**
+     * Code received from the site
+     *
+     * @var
+     */
+    private $code;
+
+    /**
+     * URL Host
+     *
+     * @var
+     */
+    private $host;
+
+    /**
+     * absolute url to the image
+     *
+     * @var
+     */
+    private $absoluteUrl;
+
+    /**
+     * Http protocol
+     *
+     * @var
+     */
+    private $scheme;
+
+    /**
+     * Not allowed image names
+     *
+     * @var array
+     */
+    private $notAllowedNames = [
+        'captcha_mod'
+    ];
 
     /**
      * Image constructor.
@@ -18,45 +70,110 @@ class Image
         $this->imageDirectory = 'src' . DIRECTORY_SEPARATOR . 'img';
     }
 
-    public function getImages(string $url, string $dir = '')
+    /**
+     * Take the code from the site
+     *
+     * @param string $url - site url
+     * @return string
+     * @throws ImageException
+     */
+    public function grabbingSite(string $url): string
     {
-        $dir = (empty($dir)) ? $this->imageDirectory : $dir;
-
-        $host = parse_url($url, PHP_URL_HOST);
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_HEADER, 0);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl, CURLOPT_URL, $url);
         $code = curl_exec($curl);
 
+        if (!$code) {
+            throw new ImageException('Не удалось соединиться с сайтом');
+        }
+
         curl_close($curl);
 
-        $this->putImages($code, $host, $dir);
+        return $code;
     }
 
+    /**
+     * Сhecking for the existence of files on a remote server
+     *
+     * @param string $path
+     * @return bool
+     */
     public function fileExists(string $path): bool
     {
         return (@fopen($path, "r") == true);
     }
 
-    public function putImages(string $code, string $host, string $dir)
+    /**
+     * Save images
+     */
+    public function saveImages()
+    {
+        $imageUrl = $this->parseCode($this->code);
+
+        for ($i=0, $count=count($imageUrl); $i<$count; $i++) {
+            $this->createAbsoluteUrl($imageUrl[$i]);
+            $this->createImage();
+        }
+    }
+
+    /**
+     * @param $imageUrl
+     */
+    public function createAbsoluteUrl($imageUrl)
+    {
+        $path = parse_url($imageUrl, PHP_URL_PATH);
+        $this->absoluteUrl = $this->scheme . $this->host . $path;
+    }
+
+    /**
+     * Copying images in the directory
+     */
+    public function createImage()
+    {
+        $name = basename($this->absoluteUrl);
+        if ($this->fileExists($this->absoluteUrl) && !in_array($name, $this->notAllowedNames)) {
+            $mime = @exif_imagetype($this->absoluteUrl);
+            if ($mime == IMAGETYPE_JPEG || $mime == IMAGETYPE_PNG || $mime == IMAGETYPE_GIF) {
+                if (!file_exists($this->imageDirectory)) {
+                    mkdir($this->imageDirectory, 0700);
+                }
+
+                copy($this->absoluteUrl, $this->imageDirectory . DIRECTORY_SEPARATOR . $name);
+            }
+        }
+    }
+
+    /**
+     * Parsing code from the site
+     *
+     * @param string $code
+     * @return array
+     */
+    public function parseCode(string $code): array
     {
         $arrayImg = [];
-        $regex = '/<\s*img[^>]*src=[\"|\'](.*?)[\"|\'][^>]*\/*>/i';
+        $regex = self::REGEX;
         preg_match_all($regex, $code, $arrayImg);
         $imageUrl = $arrayImg[1];
 
-        for ($i=0, $count = count($imageUrl); $i<$count; $i++) {
-            $path = parse_url($imageUrl[$i], PHP_URL_PATH);
-            $absolute_url = 'http://'.$host.$path;
-            $name = basename($absolute_url);
+        return $imageUrl;
+    }
 
-            if ($this->fileExists($absolute_url) && $name != 'captcha_mod') {
-                $mime = @exif_imagetype($absolute_url);
-                if ($mime == IMAGETYPE_JPEG || $mime == IMAGETYPE_PNG || $mime == IMAGETYPE_GIF) {
-                    copy($absolute_url, $dir.'/'.$name);
-                }
-            }
-        }
+    /**
+     * To receive and save images
+     *
+     * @param \string[] ...$options
+     */
+    public function getImages(string ...$options)
+    {
+        list($url, $dir) = $options;
+        $this->code = $this->grabbingSite($url);
+        $this->imageDirectory = !$dir ? $this->imageDirectory : $dir;
+        $this->host = parse_url($url, PHP_URL_HOST);
+        $this->scheme = parse_url($url, PHP_URL_SCHEME) . '://';
+
+        $this->saveImages();
     }
 }
--- tests/ImageTest.php
+++ tests/ImageTest.php
@@ -1,12 +1,44 @@
 <?php
-
 namespace Sholomka\Image\Test;
 
+use Sholomka\Image\Image;
+
+/**
+ * Class ImageTest for the test
+ * @package Sholomka\Image\Test
+ */
 class ImageTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * Name of the site for grabbing
+     */
+    const SITE = "http://pattaya.zagranitsa.com/";
 
-    public function testTrueIsTrue()
+    /**
+     * Test a connection to the site
+     */
+    public function testGrabbingSite()
     {
-        $this->assertTrue(true);
+        $class = new Image();
+        $this->assertInternalType("string", $class->grabbingSite(self::SITE));
+    }
+
+    /**
+     * Test check file
+     */
+    public function testFileExists()
+    {
+        $class = new Image();
+        $this->assertInternalType("boolean", $class->fileExists(self::SITE . DIRECTORY_SEPARATOR . "test"));
+    }
+
+    /**
+     * Parsing website code
+     */
+    public function testParseCode()
+    {
+        $class = new Image();
+        $code = $class->grabbingSite(self::SITE);
+        $this->assertInternalType("array", $class->parseCode($code));
     }
 }
